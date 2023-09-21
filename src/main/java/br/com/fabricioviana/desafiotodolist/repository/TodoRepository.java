38233d56ddebb9db01fd3a51e5baa4c166e4f532
FabriciosViana <vianasfabricio@gmail.com>
2023-09-21 11:48:02 -0300

    :sparkles:feat: Criacao do repository Todo.

---
+++ src/main/java/br/com/fabricioviana/desafiotodolist/repository/TodoRepository.java
@@ -0,0 +1,7 @@
+package br.com.fabricioviana.desafiotodolist.repository;
+
+import br.com.fabricioviana.desafiotodolist.entity.Todo;
+
+public interface TodoRepository extends JpaRepository<Todo, Long>{
+
+}
